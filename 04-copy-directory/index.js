a3c5efc564a81ac368945486c702093a563f5874
Kirill Volosevich <keriaz@mail.ru>
2022-05-21 18:45:16 +0300

    feat: task complited 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
 
+const fs = require('fs');
+const path = require('path');
+
+const pathDest = path.join(__dirname, 'files-copy');
+const pathSrc = path.join(__dirname, 'files');
+
+fs.promises.mkdir(pathDest, { recursive: true });
+fs.promises.readdir(pathSrc).then(file => {
+  file.forEach(item => {
+    fs.promises.copyFile(path.join(pathSrc,item), path.join(pathDest,item));
+  });
+ 
+  fs.promises.readdir(pathDest).then(filesDest => {
+    filesDest.forEach(itemDest => {
+      if (!file.includes(itemDest)) {
+        fs.promises.unlink(path.join(pathDest,itemDest));
+      }
+    });
+  }); 
+  console.log('Done');
+});
